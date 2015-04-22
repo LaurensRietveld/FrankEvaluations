bb8974cdb0134a2e6555ffc91b1f2a625ef080f1
Wouter Beek <w.g.j.beek@vu.nl>
2015-04-22 15:14:05 +0200

    properly clone child repos

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-
+#clone sub repos
+( cd hdt && git clone git@github.com:LaurensRietveld/hdt-cpp.git . )
+( cd frank && git clone git@github.com:LODLaundry/Frank.git . )
 
 ( cd hdt/hdt-lib && make );
 
