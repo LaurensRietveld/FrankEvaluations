373b52b7cb4c39afd1bce9941af80fdaeace76e6
Laurens Rietveld <laurens.rietveld@gmail.com>
2015-04-22 14:23:48 +0200

    added setup and experiment script

---
+++ runExperiment.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+frank=frank/frank
+rdf2hdt=hdt/hdt-lib/tools/rdf2hdt
+hdtLoad=hdt/hdt-lib/examples/load
+resultsDir=results
+tmpDir=tmp;
+
+#clean previous results
+
+rm -r $resultsDir/*
+skipped=0;
+#get all documents from frank
+$frank documents -d | while read -r downloadLink; do
+    rm -r $tmpDir/*
+    if [ `curl -I -s $downloadLink | grep .nq.gz` ]; then
+        echo "skipping quad file $downloadLink ($skipped)";
+        $((skipped++));
+        continue;
+    fi
+    
+    resultForDoc=$resultsDir/`basename $downloadLink` && mkdir -p $resultForDoc;
+    echo "Processing $downloadLink";
+    #download zip file, measure zipped file size, uncompress, and measure uncompressed file size
+    echo " - Downloading and uncompressing";
+    curl -s $downloadLink \
+        | tee >(wc -c > $resultForDoc/gzippedFileSize) \
+        | zcat \
+        | tee >(wc -c > $resultForDoc/uncompressedFileSize) >(wc -l > $resultForDoc/numTriples) > $tmpDir/unpacked;
+        
+        #build hdt file from uncompressed file. Measure hdt file size, and build time
+        echo " - Building HDT file";
+        $rdf2hdt $tmpDir/unpacked $tmpDir/hdt.hdt > $resultForDoc/hdtBuildTime;
+        wc -c $tmpDir/hdt.hdt > $resultForDoc/hdtFileSize;
+        
+        #testing load time
+        echo " - Testing load time"
+        $hdtLoad $tmpDir/hdt.hdt > $resultForDoc/hdtLoadTime;
+done;
+
+
---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+
+( cd hdt/hdt-lib && make );
+
+
+
+mkdir -p results;
+mkdir -p tmp;
