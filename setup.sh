a023ef8183fd392fd215e461e13a46105e505cd0
= <laurens.rietveld@gmail.com>
2015-04-26 17:16:15 +0200

    extended setup script with node commands

--- setup.sh
+++ setup.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 #clone sub repos
-if [ ! "$(ls -A hdt)" ]; then ( cd hdt && git clone git@github.com:LaurensRietveld/hdt-cpp.git . ); fi
-if [ ! "$(ls -A frank)" ]; then ( cd frank && git clone git@github.com:LODLaundry/Frank.git  . ); fi
+if [ ! "$(ls -A hdt)" ]; then ( cd hdt && git clone git@github.com:LaurensRietveld/hdt-cpp.git . > /dev/null); fi
+if [ ! "$(ls -A frank)" ]; then ( cd frank && git clone git@github.com:LODLaundry/Frank.git  . > /dev/null); fi
 
 #compile c
-( cd hdt/hdt-lib && make );
+( cd hdt/hdt-lib && make > /dev/null);
 
+#install npm dependencies
 
+( cd ldAdoptionExperiments && npm update );
 
 mkdir -p results;
 mkdir -p tmp;
