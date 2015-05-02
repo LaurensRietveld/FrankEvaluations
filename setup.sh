f26dbe8ea30f35cedc148f7dad8db19d051324e0
= <laurens.rietveld@gmail.com>
2015-05-02 12:39:53 +0200

    added sanity checks for node and npm

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+
+command -v npm >/dev/null 2>&1 || echo "npm is not installed. Running the ld adoption experiments is not possible"
+command -v node >/dev/null 2>&1 || echo "node is not installed. Running the ld adoption experiments is not possible"
+
+
 #clone sub repos
 echo "Cloning the HDT repository"
 if [ ! "$(ls -A hdt)" ]; then ( cd hdt && git clone git@github.com:LaurensRietveld/hdt-cpp.git . > /dev/null); fi
@@ -12,6 +17,9 @@ if [ ! "$(ls -A frank)" ]; then ( cd frank && git clone git@github.com:LODLaundr
 echo "Compiling the HDT library"
 ( cd hdt/hdt-lib && make > compileLog.log 2> compileLog.err);
 [ ! $? -eq 0 ] && echo "Failed to compile HDT. Check out the compile log (hdt/compileLog.[log|err] on what went wrong. For the list of hdt dependencies, see https://github.com/rdfhdt/hdt-cpp/tree/master/hdt-lib"
+
+
+
 
 #install npm dependencies
 echo "Initializing the NodeJs code for running LD adoption experiments"
