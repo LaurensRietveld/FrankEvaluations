563232be5b62e15e309275b17fa24578f2f7b8da
= <laurens.rietveld@gmail.com>
2015-05-02 12:02:56 +0200

    updated setup script

--- setup.sh
+++ setup.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
 #clone sub repos
+echo "Cloning the HDT repository"
 if [ ! "$(ls -A hdt)" ]; then ( cd hdt && git clone git@github.com:LaurensRietveld/hdt-cpp.git . > /dev/null); fi
+[ ! $? -eq 0 ] && echo "Failed to clone HDT"
+echo "Cloning the frank repository"
 if [ ! "$(ls -A frank)" ]; then ( cd frank && git clone git@github.com:LODLaundry/Frank.git  . > /dev/null); fi
+[ ! $? -eq 0 ] && echo "Failed to clone frank"
 
 #compile c
-( cd hdt/hdt-lib && make > /dev/null);
+echo "Compiling the HDT library"
+( cd hdt/hdt-lib && make > compileLog.log 2> compileLog.err);
+[ ! $? -eq 0 ] && echo "Failed to compile HDT. Check out the compile log (hdt/compileLog.[log|err] on what went wrong. For the list of hdt dependencies, see https://github.com/rdfhdt/hdt-cpp/tree/master/hdt-lib"
 
 #install npm dependencies
+echo "Initializing the NodeJs code for running LD adoption experiments"
+( cd ldAdoptionExperiments && npm update > npmUpdateLog.log 2> npmUpdateLog.err);
+[ ! $? -eq 0 ] && echo "Failed to update npm code. Check out the logs (ldAdoptionExperiments/npmUpdateLog.[log|err] on what went wrong."
 
-( cd ldAdoptionExperiments && npm update );
-
-mkdir -p results;
-mkdir -p tmp;
